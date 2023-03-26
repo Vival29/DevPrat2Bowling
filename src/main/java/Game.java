aa7893a3918479a3ad5b06686843658d1c8cb3ae
Vival29 <virginie.f.r.valentin@gmail.com>
2023-03-26 19:05:39 +0200

    4th test passed

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,18 +5,23 @@ public class Game {
     private Integer n = 0;
     private Frame frame;
     private List<Frame> frames;
-    public Game(){
+
+    public Game() {
         frames = new ArrayList<>(10);
     }
+
     public void roll(Integer pins) {
         // Reset du Frame au premier lancer
-        if(n%2 == 0) // paire (1er aussi)
+        if (n % 2 == 0) // paire (1er aussi)
         {
             frame = new Frame();
         }
         frame.addRoll(pins);
 
-        if(pins == 10 || frame.getRolls().size() == 2 ) { //|| (!frames.isEmpty() && frame.getRolls().size() == 1)
+        if (pins == 10) {
+            frames.add(frame);
+            n++;
+        } else if (frame.getRolls().size() == 2) {
             frames.add(frame);
         }
         n++;
@@ -26,11 +31,11 @@ public class Game {
         int totalScore = 0;
 
         for (int i = 0; i < 10; i++) {
-            //if(frames.get(i).getRolls().get(0) == 10 ){
-                //totalScore += 10 + frames.get(i+1).getScoreFrame();
-            if(frames.get(i).getScoreFrame() == 10 ){
-                totalScore += 10 + frames.get(i+1).getRolls().get(0);
-            }else {
+            if (frames.get(i).getRolls().get(0) == 10) {
+                totalScore += 10 + frames.get(i + 1).getScoreFrame();
+            } else if (frames.get(i).getScoreFrame() == 10 && frames.get(i).getRolls().size() == 2) {
+                totalScore += 10 + frames.get(i + 1).getRolls().get(0);
+            } else {
                 totalScore += frames.get(i).getScoreFrame();
             }
         }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -32,6 +32,18 @@ public class GameTest {
         }
         Assertions.assertEquals(16 ,g.score() );
     }
+    @Test
+    public void twoRoll_givenStrikePlus3Plus4_shouldGive24(){
+        int points =0;
+        Game g = new Game();
+        g.roll(10);
+        g.roll(3);
+        g.roll(4);
+        for(int i = 0; i<16; i++){
+            g.roll(0);
+        }
+        Assertions.assertEquals(24 ,g.score() );
+    }
 
 
 }
